d5f8095a324bfd79616f74d972d0f92cd55246b5
Mathias <email@example.com>
2023-12-06 10:16:41 +0100

    break

---
+++ .docker/files/firefox/autoconfig.js
@@ -0,0 +1,7 @@
+// FROM HERE last entry
+// https://support.mozilla.org/en-US/questions/1296613
+// https://support.mozilla.org/en-US/kb/customizing-firefox-using-autoconfig
+// auf deutsch - in german
+// https://support.mozilla.org/de/kb/firefox-mit-autoconfig-anpassen
+lockPref("trailhead.firstrun.branches", "nofirstrun-empty");
+lockPref("browser.aboutwelcome.enabled", false);
